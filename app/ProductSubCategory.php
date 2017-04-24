cc27162710faf6186c6cf00046eae79d54c13cca
Sunny <sunny_kumar@rvtechnologies.co.in>
2017-04-24 14:14:48 +0530

    24 april 2017

--- app/ProductSubCategory.php
+++ app/ProductSubCategory.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductSubCategory extends Model {
+    
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
