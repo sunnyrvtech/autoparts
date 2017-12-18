1c827d2ef51aa5b14e92ad2eaaf6e238af56cdc1
sunny <sunny_kumar@rvtechnologies.co.in>
2017-12-18 15:23:16 +0530

    18-12-2017

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,23 +27,19 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule) {
         $schedule->call(function () {
-            Log::error("Cron runing " . date('H:i:s'));
+            Log::error("Cron running " . date('H:i:s'));
             $order_data = DB::table('order_emails')->get();
-            Mail::send('auth.emails.welcome',['name' => 'sunnnnnnnnnnnnnnn'], function($message) {
-                $message->from('jerhica.pe@gmail.com', "Welcome To Autolighthouse");
-                $message->to('sunny_kumar@rvtechnologies.co.in')->subject('Autolighthouse Store:New Order');
-            });
-//            if ($order_data->toArray()) {
-//                foreach ($order_data as $value) {
-//                    $data = json_decode($value->order_data);
-//
-//                    Mail::send('auth.emails.status_invoice', $data, function($message) use ($data) {
-//                        $message->from('jerhica.pe@gmail.com', " Welcome To Autolighthouse");
-//                        $message->to($data['email'])->subject('Autolighthouse Store:New Order #' . $data['item_data']['id']);
-//                    });
-//                }
-//                DB::table('order_emails')->delete();
-//            }
+            if ($order_data->toArray()) {
+                foreach ($order_data as $value) {
+                    $data = json_decode($value->order_data);
+                    Log::error("Cron running " . $value->order_data);
+                    Mail::send('auth.emails.status_invoice', $data, function($message) use ($data) {
+                        $message->from('jerhica.pe@gmail.com', " Welcome To Autolighthouse");
+                        $message->to($data['email'])->subject('Autolighthouse Store:New Order #' . $data['item_data']['id']);
+                    });
+                }
+                DB::table('order_emails')->delete();
+            }
         })->everyFiveMinutes();
     }
 
